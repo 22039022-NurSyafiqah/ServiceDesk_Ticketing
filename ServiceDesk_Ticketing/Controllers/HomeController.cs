89f2142117d275785666eb6cc661031bb6aade7f
22003271-HtetEaindraLwinOo <22003271@myrp.edu.sg>
2024-07-03 10:33:43 +0800

    UI for create tickets

--- ServiceDesk_Ticketing/Controllers/HomeController.cs
+++ ServiceDesk_Ticketing/Controllers/HomeController.cs
@@ -28,7 +28,93 @@ namespace ServiceDesk_Ticketing.Controllers
             ViewData["Message"] = "You clicked Privacy!";
             return View();
         }
+        public IActionResult FaultReport()
+        {
+            return View();
+        }
+       
+        public IActionResult SubmitFaultReport(string category)
+        {
+            if (category == "category1")
+            {
+                return RedirectToAction("ClassroomCartParkingBay");
+            }
+            else if (category == "category2")
+            {
+                return RedirectToAction("EquipmentToUser");
+            }
+            return RedirectToAction("FaultReport");
+        }
 
+        public IActionResult ClassroomCartParkingBay()
+        {
+            return View();
+        }
+
+        public IActionResult EquipmentToUser()
+        {
+            return View();
+        }
+
+        public IActionResult ITServiceSupportRequest()
+        {
+            return View();
+        }
+
+        public IActionResult PrintingQuota()
+        {
+            return View();
+        }
+
+        public IActionResult EventSupport()
+        {
+            return View();
+        }
+
+        public IActionResult RequestForEquipment()
+        {
+            return View();
+        }
+
+        public IActionResult AccountActivation()
+        {
+            return View();
+        }
+        public IActionResult AppSoftwareInstallation()
+        {
+            return View();
+        }
+        public IActionResult FacebookPost()
+        {
+            return View();
+        }
+        public IActionResult WebsiteUpdate()
+        {
+            return View();
+        }
+        public IActionResult SubmitCategory(string category)
+        {
+            if (category == "category1")
+            {
+                return RedirectToAction("FaultReport");
+            }
+            else if (category == "category2")
+            {
+                return RedirectToAction("ITServiceSupportRequest");
+            }
+            else if (category == "category3")
+            {
+                return RedirectToAction("FacebookPost");
+            }
+            else if (category == "category4")
+            {
+                return RedirectToAction("WebsiteUpdate");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
